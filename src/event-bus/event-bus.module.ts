25e0d309a5c7b6c579865e5981113a10272e463f
abhishek-7262 <abhishek.7262020269@gmail.com>
2026-06-27 20:13:35 +0530

    minor fixes

--- src/event-bus/event-bus.module.ts
+++ src/event-bus/event-bus.module.ts
@@ -1,9 +1,10 @@
 import { Module, Global } from '@nestjs/common';
-import { RabbitMQService } from 'src/event-bus/rabbitmq/rabbitmq.service';
-import { PublisherService } from 'src/event-bus/services/publisher.service';
-import { SubscriberService } from 'src/event-bus/services/subscriber.service';
-import { RetryService } from 'src/event-bus/services/retry.service';
-import { EventBusService } from 'src/event-bus/services/event-bus.service';
+//import { RabbitMQService } from 'src/event-bus/rabbitmq/rabbitmq.service';
+import { RabbitMQService } from './rabbitmq/rabbitmq.service';
+import { PublisherService } from './services/publisher.service';
+import { SubscriberService } from './services/subscriber.service';
+import { RetryService } from './services/retry.service';
+import { EventBusService } from './services/event-bus.service';
 
 @Global()
 @Module({
